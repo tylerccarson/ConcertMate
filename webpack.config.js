96b12902ec87463f5a67a566a7b6f65ce91fc630
Hack Reactor Student <student@hackreactor.com>
2017-08-08 16:02:37 -0700

    testing git workflow

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 var SRC_DIR = path.join(__dirname, 'client/src');
 var DIST_DIR = path.join(__dirname, 'client/dist');
 
+
  module.exports = {
      entry: `${SRC_DIR}/index.jsx`,
      output: {
