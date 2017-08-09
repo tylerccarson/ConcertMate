d6b0f3d68f0b1e5daa2e6b503df85a2b72cff8f0
Hack Reactor Student <student@hackreactor.com>
2017-08-09 16:50:57 -0700

    map rendering

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
+const style = {
+  width: '100%',
+  height: '100%',
+  left: 160,
+  top: 160,
+  margin: 0,
+  position: 'absolute',
+  overflow: 'visible',
+  padding: 0
+}
 class Map extends React.Component {
   constructor(props) {
     super(props);
@@ -12,10 +22,12 @@ class Map extends React.Component {
 
   render() {
     return (
+      <div style={style}>
       <GoogleMapReact
         defaultCenter={this.state.center}
         defaultZoom={this.state.zoom}
       />
+      </div>
       )
   }
 }
