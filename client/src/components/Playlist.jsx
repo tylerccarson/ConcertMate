1fb5b70eb0d378f41d23b407054bd44b3810b1ed
tyler carson <tyler.c.carson@gmail.com>
2017-08-09 20:31:43 -0700

    add form for testing posts

--- client/src/components/Playlist.jsx
+++ client/src/components/Playlist.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
+import axios from 'axios';
 
-const Playlist = () => {
+class Playlist extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			artist: ''
+		}
+	}
 
-  return (
-    <div>
-    	<iframe src='https://open.spotify.com/embed?uri=spotify:user:1211115253:playlist:2GKsWTId44AS4ZaeHKnowP&theme=black'
-    					width="300" height="120"
-    					frameborder="0" allowtransparency="true"></iframe>
-    </div>
-  )
+	handleArtistEntry(artistEntryEvent) {
+		this.setState({
+			artist: artistEntryEvent.target.value
+		});
+	}
+
+	handleFormSubmit(formSubmitEvent) {
+		formSubmitEvent.preventDefault();
+		let data = {
+			artist: this.state.artist
+		};
+		axios.post('/spotify', data)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	render() {
+	  return (
+	  	<div>
+		    <div>
+		    	<form action='/' method='post' onSubmit={this.handleFormSubmit.bind(this)}>
+		    		<label>
+		    			Here for testing purposes:
+		    			<input type="text" value={this.state.artist} onChange={this.handleArtistEntry.bind(this)}/>
+		    		</label>
+		    	</form>
+		    </div>
+		    <div>
+		    	<iframe src='https://open.spotify.com/embed?uri=spotify:user:1211115253:playlist:2GKsWTId44AS4ZaeHKnowP&theme=black'
+  					width="300" height="120"
+  					frameborder="0" allowtransparency="true"></iframe>
+		    </div>
+	    </div>
+	  )		
+	}
 }
 
 export default Playlist;
