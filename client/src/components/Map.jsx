3c1195d87091dc3ccfcb9f30601d5d021dd68a02
Hack Reactor Student <student@hackreactor.com>
2017-08-09 19:17:34 -0700

    ????

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import GoogleMapReact from 'google-map-react';
+import GoogleMapMarkers from 'google-map-react';
 
 const style = {
-  width: '100%',
-  height: '100%',
+  width: '80%',
+  height: '80%',
   left: 160,
   top: 160,
   margin: 0,
@@ -18,16 +19,25 @@ class Map extends React.Component {
     super(props);
     this.state = {
       center: {lat: 37.783607, lng:-122.408967},
-      zoom: 8
+      zoom: 16,
+      markers: []
     }
   }
-
+  
+  handleClick(event) {
+    this.setState((prevState) => {
+      return {markers: prevState.markers.concat([[event.lat, event.lng]])};
+    })
+  }
+  
   render() {
+    console.log(this.state.markers);
     return (
       <div style={style}>
       <GoogleMapReact
         defaultCenter={this.state.center}
         defaultZoom={this.state.zoom}
+        onClick={this.handleClick.bind(this)}
       />
       </div>
       )
