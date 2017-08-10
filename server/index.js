d3eff3dd648da0501ad1e55c3384f5212ac1e16e
Hack Reactor Student <student@hackreactor.com>
2017-08-10 12:03:46 -0700

    work on songkick api handler

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ let app = express();
 let bodyParser = require('body-parser');
 let db = require('../database/index.js');
 let spotifyRouter = require('./spotify-router.js');
-
+let songkickRouter = require('./songkick-router.js');
 
 let port = process.env.PORT || 8888;
 
@@ -11,6 +11,9 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 //spotify routes
 app.use('/spotify', spotifyRouter);
+
+// songkick routes
+app.use('/songkick', songkickRouter);
 
 app.get('/', (req, res, next) => {
   res.status(200);
