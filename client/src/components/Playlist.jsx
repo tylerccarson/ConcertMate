ac67ab05f8bf12ff724223595285470a976402cc
tyler carson <tyler.c.carson@gmail.com>
2017-08-10 12:32:28 -0700

    spotify

--- client/src/components/Playlist.jsx
+++ client/src/components/Playlist.jsx
@@ -20,6 +20,63 @@ class Playlist extends React.Component {
 			.catch((error) => {
 				console.log(error);
 			})
+		//these two steps may be unnecessary for time being
+			// 4. request refresh and access tokens
+			// 5. tokens returned to app
+			// 6 and 7: do work
+		
+// 		// 1. get request for authorization
+// 		axios.get('https://accounts.spotify.com/authorize', {
+// 			headers: spotifyHeaders,
+// 			params: {
+// 				// 2. do so with authorized scopes
+// 				client_id: spotifyCredentials.client_id,
+// 				response_type: 'code',
+// 				redirect_uri: spotifyCredentials.redirect_URIs[1]
+// 			}
+// 		})
+// 		// 3. user redirected back to redirect URI
+// 			//  how to deal with redirect? perhaps build route outside of component will mount. This should only be for 1st step
+// 		.then((response) => {
+// 			console.log('code for access token: ', response.code);
+// 		})
+// 		.catch((error) => {
+// 			console.log('authorization error: ', error);
+// 		});
+
+
+// 		//// this promise chain will fetch the user id, create a playlist, then set the state for those two props
+// 		// axios({
+// 		// 	url: 'https://api.spotify.com/v1/me',
+// 		// 	method: 'get',
+// 		// 	headers: spotifyHeaders
+// 		// })
+// 		// //create playlist
+// 		// .then((userId) => {
+// 		// 	console.log('got userId: ', userId);
+// 		// 	this.setState({
+// 		// 		userId: userId
+// 		// 	});
+
+// 		// 	return axios({
+// 		// 		url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+// 		// 		method: 'post',
+// 		// 		headers: spotifyHeaders,
+// 		// 		data: {
+// 		// 			name: 'ConcertMate'
+// 		// 		}
+// 		// 	})
+// 		// .then((playlist) => {
+// 		// 	//setstate
+// 		// 	this.setState({
+// 		// 		playlistId: playlist.id
+// 		// 	})
+
+// 		// 	console.log('Playlist created', playlist);
+// 		// })
+// 		// });
+
+// >>>>>>> spotify
 	}
 
 	handleArtistEntry(artistEntryEvent) {
