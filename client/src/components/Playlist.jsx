11598cb2e66e7135ae15672286cc283b76ec07b0
tyler carson <tyler.c.carson@gmail.com>
2017-08-09 22:13:30 -0700

    add axios requests to create new playlist on mount, update playlist upon submitting new artist

--- client/src/components/Playlist.jsx
+++ client/src/components/Playlist.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
 import axios from 'axios';
 
+let accessToken = 'BQCMwC19LrnRoyhPTiUbi5gFdrFM584B5xcLvVgZrcrlf3cFYEIyxVsVYvpmeK2qCscz9iiBtk_qjm8aKh6a-Q3qyg63LUU0sABKLXswc68Lu9AimAkvu58EEdm53eQrjrvzPI3a0Nxgviuam89CajzKU5Z7emO2NiRI1WH_XG0MIIaT72JUci7mdU6BPsggJ5SbpWwkmgCVbbvtcyR4yIK2gBDH1oZAdYrWPMXaf6QoN_O_V4MhA79thd1Ye1633YvspMzJ7iW6wm5AHj8';
+
+let spotifyHeaders = {
+	'Authorization': 'Bearer ' + accessToken
+};	
+
 class Playlist extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			artist: ''
+			artist: '',
+			userId: null,
+			playlistId: null
+		}
+	}
+
+	componentWillMount() {
+		//get userID
+		axios({
+			url: 'https://api.spotify.com/v1/me',
+			method: 'get',
+			headers: spotifyHeaders
+		})
+		//create playlist
+		.then((userId) => {
+
+			this.setState({
+				userId: userId
+			});
+
+			return axios({
+				url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+				method: 'post',
+				headers: spotifyHeaders,
+				data: {
+					name: 'ConcertMate'
+				}
+			})
+		.then((playlist) => {
+			//setstate
+			this.setState({
+				playlistId: playlist.id
+			})
+
+			console.log('Playlist created', playlist);
+		})
 		}
 	}
 
