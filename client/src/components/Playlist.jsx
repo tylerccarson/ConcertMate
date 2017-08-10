5a5c4d1304ef2c00e94424ddf43001d4aba3ef28
tyler carson <tyler.c.carson@gmail.com>
2017-08-10 12:02:46 -0700

    pseudocode for authorization

--- client/src/components/Playlist.jsx
+++ client/src/components/Playlist.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import axios from 'axios';
 
+let spotifyCredentials = {
+	client_id: '1b4dd6acf0c14120b5fa6ae37b4c773a',
+	client_secret: '365aec3923fe452fbbeb31fe842c2a4c',
+	redirect_URIs: ['https://concertmate.herokuapp.com/callback', 'http://localhost:8888/callback']
+};
+
 let accessToken = 'BQCMwC19LrnRoyhPTiUbi5gFdrFM584B5xcLvVgZrcrlf3cFYEIyxVsVYvpmeK2qCscz9iiBtk_qjm8aKh6a-Q3qyg63LUU0sABKLXswc68Lu9AimAkvu58EEdm53eQrjrvzPI3a0Nxgviuam89CajzKU5Z7emO2NiRI1WH_XG0MIIaT72JUci7mdU6BPsggJ5SbpWwkmgCVbbvtcyR4yIK2gBDH1oZAdYrWPMXaf6QoN_O_V4MhA79thd1Ye1633YvspMzJ7iW6wm5AHj8';
 
 let spotifyHeaders = {
@@ -20,35 +26,50 @@ class Playlist extends React.Component {
 	componentWillMount() {
 		// getting error due to unauthorization. Need to incorporate client secret ID and refresh token capability.
 		// guide here: https://developer.spotify.com/web-api/authorization-guide/
-		axios({
-			url: 'https://api.spotify.com/v1/me',
-			method: 'get',
-			headers: spotifyHeaders
-		})
-		//create playlist
-		.then((userId) => {
-			console.log('got userId: ', userId);
-			this.setState({
-				userId: userId
-			});
+		// 1. get request for authorization
+		// 2. do so with authorized scopes
+		// 3. user redirected back to redirect URI
+			//  how to deal with redirect? perhaps build route outside of component will mount. This should only be for 1st step
 
-			return axios({
-				url: `https://api.spotify.com/v1/users/${userId}/playlists`,
-				method: 'post',
-				headers: spotifyHeaders,
-				data: {
-					name: 'ConcertMate'
-				}
-			})
-		.then((playlist) => {
-			//setstate
-			this.setState({
-				playlistId: playlist.id
-			})
+			//these two steps may be unnecessary for time being
+		// 4. request refresh and access tokens
+		// 5. tokens returned to app
+		// 6 and 7: do work
+		
+		
 
-			console.log('Playlist created', playlist);
-		})
-		});
+
+		//// this promise chain will fetch the user id, create a playlist, then set the state for those two props
+		// axios({
+		// 	url: 'https://api.spotify.com/v1/me',
+		// 	method: 'get',
+		// 	headers: spotifyHeaders
+		// })
+		// //create playlist
+		// .then((userId) => {
+		// 	console.log('got userId: ', userId);
+		// 	this.setState({
+		// 		userId: userId
+		// 	});
+
+		// 	return axios({
+		// 		url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+		// 		method: 'post',
+		// 		headers: spotifyHeaders,
+		// 		data: {
+		// 			name: 'ConcertMate'
+		// 		}
+		// 	})
+		// .then((playlist) => {
+		// 	//setstate
+		// 	this.setState({
+		// 		playlistId: playlist.id
+		// 	})
+
+		// 	console.log('Playlist created', playlist);
+		// })
+		// });
+
 	}
 
 	handleArtistEntry(artistEntryEvent) {
