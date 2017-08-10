f9ce2403a18828b1e3d6abea979f4073f7e04175
tyler carson <tyler.c.carson@gmail.com>
2017-08-09 20:46:53 -0700

    send post request with artist to router

--- server/spotify-router.js
+++ server/spotify-router.js
@@ -1,5 +1,10 @@
 let router = require('express').Router();
 let axios = require('axios');
+let bodyParser = require('body-parser');
 
+router.post('/', bodyParser.json(), (req, res) => {
+	console.log(req.body);
+	res.send('got artist post');
+})
 
 module.exports = router;
