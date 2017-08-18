2c066e397f9477bfae2742d2fe48304a05e9725f
tyler carson <tyler.c.carson@gmail.com>
2017-08-18 12:40:01 -0700

    add datepicker

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//what does this component need? Needs to be a form that allows for search based on date, location?, event type, also possible by venue and artist, which require slightly different api paths
+//bootstrap will allow for easier for handling than raw react
+
 class Favorites extends React.Component {
 
 	render() {
