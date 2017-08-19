fa2c414b15722cdb769826eb2503c86135b49f02
Ting Wang <twang1225@gmail.com>
2017-08-18 17:28:43 -0700

    reorganize event info

--- client/src/components/ConcertEntry.jsx
+++ client/src/components/ConcertEntry.jsx
@@ -4,7 +4,7 @@ const ConcertEntry = (props) => {
 	return (
 		<div>
       <span>{props.event.performance[0].displayName}</span>
-      <span> Venue: {props.event.venue.displayName} {props.event.start.date} {props.event.start.time}</span>
+      <span> at {props.event.venue.displayName} {props.event.start.date} {props.event.start.time}</span>
 			<a href={props.event.uri}> Buy Tickets</a>
     </div>
 	)
