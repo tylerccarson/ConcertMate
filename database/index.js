536a90108c0247c7bec080ec1e1867f0fdb421f6
tyler carson <tyler.c.carson@gmail.com>
2017-08-20 15:18:45 -0700

    begin implementing database for events

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const credentials = require('./config.js');
 let user = credentials.loginData.user;
 let password = credentials.loginData.password
+
 const Sequelize = require('sequelize');
 const seq = new Sequelize('greenfielddb', user, password, {
   host: 'localhost',
@@ -15,3 +16,24 @@ seq
   .catch(err => {
     console.log('error connecting to DB ', err);
   });
+
+const Events = seq.define('events', {
+  displayName: Sequelize.STRING,
+  headline: Sequelize.STRING,
+  uri: Sequelize.STRING,
+  time: Sequelize.TIME,
+  date: Sequelize.DATEONLY,
+  venue: Sequelize.STRING,
+  latitude: Sequelize.INTEGER,
+  longitude: Sequelize.INTEGER
+});
+
+let createEvent = Events.create({
+	//fill in attribues
+});
+
+let getEvents = function(date) {
+	Events.findAll({
+		date: date
+	});
+};
--- server/songkick-router.js
+++ server/songkick-router.js
@@ -3,12 +3,22 @@ let router = express.Router();
 let axios = require('axios');
 let apiKey = 'qRDqWCS0qJpDH4Qp';
 let bodyParser = require('body-parser');
+//let db = require('../database/index.js');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
 router.post('/', (req, res) => {
 	let date = req.body.date;
+  console.log(date);
+  // check db for concert data for data
+    //if there is data, then send that data back to the client
+    //else, do the api call
+      //put results into the db
+
+
+
+
 	let url  = `http://api.songkick.com/api/3.0/events.json?apikey=${apiKey}&location=geo:37.783607,-122.408967&min_date=${date}&max_date=${date}`;
   axios.get(url)
     .then((events) => {
