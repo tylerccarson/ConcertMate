a465395ee5f9bc8252129f22a8da8eb8d9580662
Karun Pavithran <karunpavithran6892@gmail.com>
2017-08-21 10:42:10 -0700

    work on search bar

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -1,29 +1,53 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import {Navbar, FormGroup, FormControl, Button} from 'react-bootstrap';
+import axios from 'axios';
 
 class Favorites extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
 			startDate: moment(),
-      radius: 5
+      radius: 5,
+      search: ''
 		}
 	}
+
+  handleSearch(text) {
+    console.log('text ', event.target.value);
+    this.setState({
+      search: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    
+  }
 
 	render() {
   	
     return (
     	<div>
-	    	<div>
-	    		Pick a day:
-	    		<DatePicker
-						dateFormat="MM/DD/YYYY"
-						selected={this.props.startDate}
-						onChange={this.props.handleDateChange}
-					/>
-				</div>
-    	</div>
+        <div>
+          Pick a day:
+          <DatePicker
+            dateFormat="MM/DD/YYYY"
+            selected={this.props.startDate}
+            onChange={this.props.handleDateChange}
+          />
+        </div>
+
+          <Navbar>
+            <Navbar.Form pullLeft>
+              <FormGroup>
+                <FormControl type="text" placeholder="Enter starting location" onChange={this.handleSearch}/>
+              </FormGroup>
+              {' '}
+              <Button type="submit" onClick={this.handleSubmit}>Submit</Button>
+            </Navbar.Form>
+          </Navbar>
+        </div>
     )		
 	}
 };
