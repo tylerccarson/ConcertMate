c0582ac2a403593aac8e8fdbd05d673915bec9b1
tyler carson <tyler.c.carson@gmail.com>
2017-08-20 19:43:35 -0700

    delete unncecessary helper file

--- server/spotify-helpers.js
+++
@@ -1,9 +0,0 @@
-module.exports.parseTracks = function(trackList) {
-	let parsedList = {
-		uris: []
-	}
-
-	//do some things to turn the array of track objects into an array on trackURI strings for API
-
-	return parsedList;
-}
