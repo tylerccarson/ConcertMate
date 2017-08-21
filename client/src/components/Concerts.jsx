102b6f67d6c0c3f36a0f5d1fab2533594dcfd9ee
Ting Wang <twang1225@gmail.com>
2017-08-20 19:42:53 -0700

    rebase

--- client/src/components/Concerts.jsx
+++ client/src/components/Concerts.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import axios from 'axios';
 import ConcertEntry from './ConcertEntry.jsx';
 import {ListGroup} from 'react-bootstrap';
+import {Panel} from 'react-bootstrap';
 
 const Concerts = (props) => {
-	return (
-    <ListGroup>
-    	{props.events.map((event, i) => {
-    		return <ConcertEntry handleHover={props.handleHover} event={event} key={i} handleArtistClick={props.handleArtistClick}/>
-    	})}
-    </ListGroup>
+
+  return (
+    <Panel collapsible defaultExpanded header="Nearby Concerts" bsStyle="info">
+      <ListGroup fill>
+        {props.events.map((event, i) => {
+          return <ConcertEntry handleHover={props.handleHover} event={event} key={i} handleArtistClick={props.handleArtistClick}/>
+        })}
+      </ListGroup>
+    </Panel>
+
   )
 }
 
